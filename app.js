5df2bec83eca3a8533a1cfc61d1820eefcc96d6a
Rai017 <165829336+Rai017@users.noreply.github.com>
2024-06-03 22:13:51 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+gsap.to(".imagec",{
+    ease: Expo.easeInOut,
+    width: "100%",
+    duration:2,
+    stagger:2
+})
+gsap.to(".text h1",{
+    ease: Expo.easeInOut,
+    top:0,
+    stagger:2
+})
+gsap.to(".text h1",{
+    ease: Expo.easeInOut,
+    delay:1,
+    top:"-100%",
+    stagger:2
+})
